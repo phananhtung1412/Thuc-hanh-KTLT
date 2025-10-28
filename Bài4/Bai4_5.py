8dcaee20ceb5e0f64a7a69a8c66dc03a8e14472e
phan anh tùng (245752021610151) <phananhtung108@gmail.com>
2025-10-28 16:13:07 +0700

    Add files via upload

---
+++ Bài4/Bai4_5.py
@@ -0,0 +1,3 @@
+ds = input("Danh sách: ").split() 
+for so in reversed(ds): 
+    print(so) 
---
+++ Bài4/Bai4_6.py
@@ -0,0 +1,3 @@
+hoten = input("Nhập họ và tên: ").split() 
+print("Họ:", hoten[0]) 
+print("Tên:", hoten[-1]) 
